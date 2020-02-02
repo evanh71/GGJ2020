ad4bf4f10379cf3314b2f73c764343f17aa71da5
Eric Halina <kaifinlol@gmail.com>
2020-02-02 16:18:32 -0700

    The Final Countdown

---
+++ TimerH.gd
@@ -0,0 +1,6 @@
+extends Timer
+
+
+
+func _on_Timer_timeout():
+	get_tree().change_scene('res://credits.tscn')
--- enddia.gd
+++ enddia.gd
@@ -1,13 +1,26 @@
 extends RichTextLabel
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+export var dialog = ['Seems quiet...']
+var page = 0
+var decision_time = false
+var leave_time = false
 
+signal black
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	set_bbcode(dialog[page])
+	set_visible_characters(0)
+	set_process_input(true)
+	
+func _input(event):
+	# handles the input from the player
+	
+	if event is InputEventKey and event.pressed:
+		# if the space bar is pressed, advance the text at the end of the messa
+		if event.scancode == KEY_SPACE:
+			if get_visible_characters() > get_total_character_count():
+				get_tree().change_scene('res://credits.tscn')
+	
+func _on_Timer_timeout():
+	# every 0.05 seconds, display a new character
+	set_visible_characters(get_visible_characters()+1)
